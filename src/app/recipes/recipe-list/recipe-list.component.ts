1ef7ff0f6cdfdd9fd9d77fd9d2ee8abe5ea71e25
Mousumi <dutta.mousumi136@gmail.com>
2024-02-13 18:21:17 -0500

    hamburger menu added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-recipes=[];
+export class RecipeListComponent implements OnInit{
+recipes: Recipe[]=[
+  new Recipe('Chicken curry', 'Gravy with Chicken and spices','https://www.allrecipes.com/thmb/Jg10XCt9o-5_eXvdpx3BLU30rv4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1053643-295634ecf1344093987892a016024411.jpg')
+];
+constructor(){}
+ngOnInit(){}
 }
