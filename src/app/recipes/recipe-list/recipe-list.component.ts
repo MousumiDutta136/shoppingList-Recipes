f23fc1302604514baf45bb914c42dcafe0766ea5
Mousumi <dutta.mousumi136@gmail.com>
2024-02-14 11:52:31 -0500

    created ingredient model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit{
 recipes: Recipe[]=[
+  new Recipe('Chicken curry', 'Gravy with Chicken and spices','https://www.allrecipes.com/thmb/Jg10XCt9o-5_eXvdpx3BLU30rv4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1053643-295634ecf1344093987892a016024411.jpg'),
   new Recipe('Chicken curry', 'Gravy with Chicken and spices','https://www.allrecipes.com/thmb/Jg10XCt9o-5_eXvdpx3BLU30rv4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1053643-295634ecf1344093987892a016024411.jpg')
 ];
 constructor(){}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Ingredient } from '../shared/ingrediant.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+ingredients: Ingredient[] = []
 }
