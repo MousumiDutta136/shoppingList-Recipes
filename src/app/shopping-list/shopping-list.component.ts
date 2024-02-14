68d114027021e99f1cf2ef905bd8900d1b97706e
Mousumi <dutta.mousumi136@gmail.com>
2024-02-14 14:33:11 -0500

    adding button add, delete and edit in shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,5 +6,12 @@ import { Ingredient } from '../shared/ingrediant.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-ingredients: Ingredient[] = []
+ingredients: Ingredient[] = [
+new Ingredient('Chicken', 2),
+new Ingredient('Green Chillies', 4),
+new Ingredient('Cumin powder', 1),
+new Ingredient('Tomato', 2),
+new Ingredient('Cardamom', 3)
+
+];
 }
