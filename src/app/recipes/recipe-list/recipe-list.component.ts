df23826120580ea30b1cc997bd2a1b69a588cb08
Mousumi <dutta.mousumi136@gmail.com>
2024-02-18 07:33:09 -0500

    communication between components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit{
+  @Output() recipeWasSelected=new EventEmitter<Recipe>();
+
 recipes: Recipe[]=[
-  new Recipe('Chicken curry', 'Gravy with Chicken and spices','https://www.allrecipes.com/thmb/Jg10XCt9o-5_eXvdpx3BLU30rv4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1053643-295634ecf1344093987892a016024411.jpg'),
-  new Recipe('Chicken curry', 'Gravy with Chicken and spices','https://www.allrecipes.com/thmb/Jg10XCt9o-5_eXvdpx3BLU30rv4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1053643-295634ecf1344093987892a016024411.jpg')
+  new Recipe('Chicken curry', 'Gravy with Chicken and spices','https://www.foodandwine.com/thmb/8YAIANQTZnGpVWj2XgY0dYH1V4I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/spicy-chicken-curry-FT-RECIPE0321-58f84fdf7b484e7f86894203eb7834e7.jpg'),
+  new Recipe('Paneer butter masala', 'Gravy with Paneer and spices','https://www.yummytummyaarthi.com/wp-content/uploads/2021/12/1.jpg')
 ];
 constructor(){}
 ngOnInit(){}
+
+onRecipeSelected(recipe:Recipe){
+  this.recipeWasSelected.emit(recipe);
+}
 }
